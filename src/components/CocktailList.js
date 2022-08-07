cc37dbc600c8f62df5dc3dacbcb9847787360f8b
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-08 03:45:39 +0500

    display cocktails list into the front page

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -5,6 +5,7 @@ import { useGlobalContext } from '../context';
 
 const CocktailList = () => {
   const { cocktails, loading, setloading } = useGlobalContext();
+  const { id, name, image, info, glass } = cocktails;
 
   if (loading) {
     return <Loading />;
@@ -16,22 +17,9 @@ const CocktailList = () => {
     <section className="section">
       <h2 className="section-title">cocktails</h2>
       <div className="cocktails-center">
-        <article className="cocktail">
-          <div className="img-container">
-            <img
-              src="https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg"
-              alt="A1"
-            />
-          </div>
-          <div className="cocktail-footer">
-            <h3>A1</h3>
-            <h4>Cocktail glass</h4>
-            <p>Alcoholic</p>
-            <a className="btn btn-primary btn-details" href="/cocktail/17222">
-              details
-            </a>
-          </div>
-        </article>
+        {cocktails.map((drink) => {
+          return <Cocktail key={drink.id} {...drink} />;
+        })}
       </div>
     </section>
   );
